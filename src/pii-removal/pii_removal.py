30ba5530589066c1193e2f0a713bf37b0a1c83fa
sirlolcat <sirlolcatX@pm.me>
2023-11-27 12:51:48 +0100

    fix: improve pii-removal over combined csv

--- src/pii-removal/pii_removal.py
+++ src/pii-removal/pii_removal.py
@@ -1,104 +1,114 @@
 import os
 import re
-from concurrent.futures import ProcessPoolExecutor
 import spacy
 import pandas as pd
 from tqdm.auto import tqdm
 import matplotlib.pyplot as plt
+from concurrent.futures import ProcessPoolExecutor
 
-# Load a spaCy model with NER, disabling unnecessary components
-nlp = spacy.load('en_core_web_md', disable=['parser', 'tagger', 'lemmatizer'])
+# Constants and configurations
+NLP_MODEL = 'en_core_web_md'
+PII_LABELS = {'PERSON', 'ORG', 'CARDINAL', 'GPE', 'LOC'}
+EMAIL_REGEX = re.compile(r'\b\S+@\S+\.\S+\b')
+MAX_LENGTH = 2000000
 
-# Regex for email detection
-email_regex = re.compile(r'\b\S+@\S+\.\S+\b')
+# Initialize spaCy model with configurations
+def init_spacy_model():
+    nlp = spacy.load(NLP_MODEL, disable=['parser', 'tagger', 'lemmatizer'])
+    nlp.max_length = MAX_LENGTH
+    return nlp
 
-# Function to process a batch of texts
-def process_batch(batch):
-    results = []
-    for text in batch:
+nlp = init_spacy_model()
+
+# Function to remove PII using regex and NER
+def remove_pii(text, nlp):
+    text = re.sub(EMAIL_REGEX, '[EMAIL]', text)
+    doc = nlp(text)
+    replacements_count = 0
+    for ent in doc.ents:
+        if ent.label_ in PII_LABELS:
+            text = text.replace(ent.text, f'[{ent.label_}]')
+            replacements_count += 1
+    return text, replacements_count
+
+# Process a single row of data
+def process_row(row, text_columns, nlp):
+    row_replacements = 0
+    for col in text_columns:
+        text = row[col]
         if isinstance(text, str):
-            # Split large texts into smaller parts if they exceed a certain length
-            max_length = 1000000  # Set the max length for a part
-            parts = [text[i:i + max_length] for i in range(0, len(text), max_length)]
-            
-            combined_cleaned_text = ""
-            combined_improvement_score = 0
-            for part in parts:
-                original_length = len(part)
-                part = re.sub(email_regex, '[EMAIL]', part)
-                doc = nlp(part)
-                pii_labels = {'PERSON', 'ORG', 'CARDINAL', 'GPE', 'LOC'}
-                for ent in doc.ents:
-                    if ent.label_ in pii_labels:
-                        part = part.replace(ent.text, f'[{ent.label_}]')
-                cleaned_length = len(part)
-                combined_cleaned_text += part
-                combined_improvement_score += (original_length - cleaned_length) / original_length if original_length > 0 else 0
+            cleaned_text, replacements = remove_pii(text, nlp)
+            row[col] = cleaned_text
+            row_replacements += replacements
+    return row, row_replacements
 
-            results.append((combined_cleaned_text, 1, combined_improvement_score / len(parts)))
-        else:
-            results.append((text, 0, 0.0))
-    return results
+# Apply PII removal to a chunk of data
+def process_chunk(chunk, text_columns, nlp):
+    chunk_replacements = 0
+    for i, row in chunk.iterrows():
+        processed_row, row_replacements = process_row(row, text_columns, nlp)
+        chunk.iloc[i] = processed_row
+        chunk_replacements += row_replacements
+    return chunk, chunk_replacements
 
-def process_csv_in_chunks(file, chunksize=1000, batchsize=100):
-    cleaned_file_path = file.replace('raw', 'processed').replace('.csv', '_cleaned.csv')
+# Parallel processing of chunks
+def parallel_process_chunks(chunks, text_columns, nlp):
+    processed_chunks = []
     replacements_tracker = []
-    improvement_metric = []
+    with ProcessPoolExecutor() as executor:
+        futures = [executor.submit(process_chunk, chunk, text_columns, nlp) for chunk in chunks]
+        for future in tqdm(futures, total=len(futures), desc="Processing Chunks"):
+            processed_chunk, chunk_replacements = future.result()
+            processed_chunks.append(processed_chunk)
+            replacements_tracker.append(chunk_replacements)
+    return processed_chunks, replacements_tracker
 
-    total_rows = pd.read_csv(file, usecols=['Email Text']).shape[0]
-    total_chunks = (total_rows + chunksize - 1) // chunksize
+# Calculate improvement score based on replacements
+def calculate_improvement_score(replacements_tracker, data_length):
+    total_replacements = sum(replacements_tracker)
+    return total_replacements / (data_length * MAX_LENGTH)
 
-    for chunk_number, chunk in enumerate(pd.read_csv(file, chunksize=chunksize)):
-        desc = f"Processing Chunk {chunk_number+1}/{total_chunks}"
-        replacements = 0
-        improvement_score = 0.0
+# Process the CSV in chunks and apply PII removal
+def process_csv_in_chunks(file_path, chunksize=1000):
+    # Get text columns to process
+    sample_df = pd.read_csv(file_path, nrows=0)
+    text_columns = sample_df.select_dtypes(include=['object']).columns.tolist()
+    
+    # Read the dataset in chunks
+    chunks = [chunk for chunk in pd.read_csv(file_path, chunksize=chunksize)]
+    cleaned_file_path = file_path.replace('.csv', '_cleaned.csv')
 
-        updated_texts = []
-        with tqdm(total=len(chunk), desc=desc, unit="rows") as pbar:
-            batches = [chunk['Email Text'][i:i+batchsize] for i in range(0, len(chunk), batchsize)]
-            with ProcessPoolExecutor() as executor:
-                for batch_results in executor.map(process_batch, batches):
-                    for text, repl, score in batch_results:
-                        updated_texts.append(text)
-                        replacements += repl
-                        improvement_score += score
-                    pbar.update(len(batch_results))
+    # Process chunks in parallel
+    processed_chunks, replacements_tracker = parallel_process_chunks(chunks, text_columns, nlp)
+    
+    # Calculate the improvement score
+    improvement_score = calculate_improvement_score(replacements_tracker, len(chunks))
 
-        replacements_tracker.append(replacements)
-        improvement_metric.append(improvement_score / len(chunk) if len(chunk) > 0 else 0)
-
-        chunk['Email Text'] = updated_texts
-        mode = 'w' if chunk_number == 0 else 'a'
-        header = mode == 'w'
-        chunk.to_csv(cleaned_file_path, index=False, mode=mode, header=header)
-
-    return pd.DataFrame({
-        'chunk_index': range(len(replacements_tracker)),
-        'replacements': replacements_tracker,
-        'improvement_score': improvement_metric
-    })
+    # Write processed data back to a cleaned CSV
+    for i, processed_chunk in enumerate(processed_chunks):
+        mode = 'w' if i == 0 else 'a'
+        header = True if i == 0 else False
+        processed_chunk.to_csv(cleaned_file_path, index=False, mode=mode, header=header)
+    
+    return replacements_tracker, improvement_score
 
 # Main execution block
 if __name__ == "__main__":
     script_dir = os.path.dirname(os.path.realpath(__file__))
-    file_path = os.path.join(script_dir, '..', '..', 'data', 'raw', 'Phishing_Email.csv')
+    data_dir = os.path.join(script_dir, '..', '..', 'data', 'processed')
+    file_name = 'combined_dataset.csv'
+    file_path = os.path.join(data_dir, file_name)
 
-    chunk_size = 1000  # Adjust as needed
-    batch_size = 100  # Adjust the batch size as needed
-
-    replacements_df = process_csv_in_chunks(file_path, chunk_size, batch_size)
+    replacements_tracker, improvement_score = process_csv_in_chunks(file_path, chunksize=3000)
 
     # Plotting the results
     fig, ax1 = plt.subplots()
-
     ax2 = ax1.twinx()
-    ax1.bar(replacements_df['chunk_index'], replacements_df['replacements'], color='g')
-    ax2.plot(replacements_df['chunk_index'], replacements_df['improvement_score'], color='b')
-
+    ax1.bar(range(len(replacements_tracker)), replacements_tracker, color='g')
+    ax2.plot(range(len(replacements_tracker)), [improvement_score] * len(replacements_tracker), color='b', linestyle='--')
     ax1.set_xlabel('Chunk Index')
     ax1.set_ylabel('Number of Replacements', color='g')
-    ax2.set_ylabel('Average Improvement Score', color='b')
-
+    ax2.set_ylabel('Improvement Score', color='b')
     plt.title('PII Replacements and Improvement Score per Chunk')
-    plt.savefig(os.path.join(script_dir, '..', '..', 'docs', 'replacements_and_improvement_chart.png'))
+    plt.savefig(os.path.join(script_dir, '..', '..', 'docs', 'replacements_chart.png'))
     plt.show()
